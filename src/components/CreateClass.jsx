eb90d5a37eff8d47927e86855c4020087aab39c9
Qua Wilburn <quawilburn@Quas-MacBook-Air.local>
2021-11-15 13:49:10 -0600

    set up the change and submit functionality

--- src/components/CreateClass.jsx
+++ src/components/CreateClass.jsx
@@ -2,18 +2,51 @@ import React, { useState } from 'react';
 
 const initailFormValues = {
     name: '',
-    type: '',
-    startTime: '',
-    duration: '',
-    intensityLevel: '',
-    location: '',
+    type: false,
+    startTime: false,
+    duration: false,
+    intensityLevel: false,
+    location: false,
     attendees: '',
     maxClassSize: ''
 };
 
 const CreateClass = (props) => {
+    const {
+        values,
+        submit,
+        change,
+    } = props
 
-    const [class, setClass] = useState(initailFormValues);
+const [class, setClass] = useState(initailFormValues);
+
+
+
+const onChange = (evt) => {
+const {
+    name,
+    type,
+    startTime,
+    duration,
+    intensityLevel,
+    location,
+    attendees,
+    maxClassSize} = evt.target;
+}
+const onSubmit = (evt) => {
+// Work in progress
+    evt.preventDefault()
+    submit()
+}
+
+return(
+    <div>
+        <h2>Create Your Class</h2>
+        <form id='class-form' > 
+
+        </form>
+    </div>
+)
 }
 
 
