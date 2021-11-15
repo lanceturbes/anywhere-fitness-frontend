80947dbb66e1e1a466b61b1c31c5633d91796756
Qua Wilburn <quawilburn@Quas-MacBook-Air.local>
2021-11-15 15:11:28 -0600

    created the basic template for the instructor form

--- src/components/CreateClass.jsx
+++ src/components/CreateClass.jsx
@@ -6,47 +6,94 @@ const initailFormValues = {
     startTime: false,
     duration: false,
     intensityLevel: false,
-    location: false,
+    location: '',
     attendees: '',
     maxClassSize: ''
 };
 
-const CreateClass = (props) => {
-    const {
-        values,
-        submit,
-        change,
-    } = props
+function CreateClass(props) {
 
-const [class, setClass] = useState(initailFormValues);
+    const [createdClass, setCreatedClass] = useState(initailFormValues);
 
+    const onChange = (evt) => {
+        // work in progress
+    };
+    const onSubmit = (evt) => {
+        // Work in progress
+        evt.preventDefault();
+    };
 
-
-const onChange = (evt) => {
-const {
-    name,
-    type,
-    startTime,
-    duration,
-    intensityLevel,
-    location,
-    attendees,
-    maxClassSize} = evt.target;
-}
-const onSubmit = (evt) => {
-// Work in progress
-    evt.preventDefault()
-    submit()
-}
-
-return(
-    <div>
-        <h2>Create Your Class</h2>
-        <form id='class-form' > 
-
-        </form>
-    </div>
-)
+    return (
+        <div>
+            <h2>Create Your Class</h2>
+            <form id='class-form-container'>
+                <label>Instructor Name
+                    <input
+                        // value={name}
+                        onChange={onChange}
+                        name='name'
+                        type='text' />
+                </label>
+                <label>Class Type
+                    <select
+                        onChange={onChange}
+                        // value={type}
+                        name='type'
+                    >
+                        <option>Select a Class Type</option>
+                    </select>
+                </label>
+                <label>Start Time
+                    <select
+                        onChange={onChange}
+                        // value={startTime}
+                        name='startTime'
+                    >
+                        <option>Select a Start Time</option>
+                    </select>
+                </label>
+                <label>Duration
+                    <select
+                        onChange={onChange}
+                        // value={startTime}
+                        name='duration'
+                    >
+                        <option>Duration of Class</option>
+                    </select>
+                </label>
+                <label>Intensity Level
+                    <select
+                        onChange={onChange}
+                        // value={intensityLevel}
+                        name='intensityLevel'
+                    >
+                        <option>Select your Intensity Level</option>
+                    </select>
+                </label>
+                <label>Location
+                    <input
+                        // value={location}
+                        onChange={onChange}
+                        name='location'
+                        type='text' />
+                </label>
+                <label>Current number of registered attendees
+                    <input
+                        // value={attendees}
+                        onChange={onChange}
+                        name='attendees'
+                        type='text' />
+                </label>
+                <label>Max Class Size
+                    <input
+                        // value={maxClassSize}
+                        onChange={onChange}
+                        name='maxClassSize'
+                        type='text' />
+                </label>
+            </form>
+        </div>
+    );
 }
 
 
