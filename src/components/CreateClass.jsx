22a13e7c290b488f247a109130b441021b5c3b27
Qua Wilburn <quawilburn@Quas-MacBook-Air.local>
2021-11-15 13:00:16 -0600

    set the ininitial values for the form

--- src/components/CreateClass.jsx
+++ src/components/CreateClass.jsx
@@ -1,2 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+const initailFormValues = {
+    name: '',
+    type: '',
+    startTime: '',
+    duration: '',
+    intensityLevel: '',
+    location: '',
+    attendees: '',
+    maxClassSize: ''
+};
+
+const CreateClass = (props) => {
+
+    const [class, setClass] = useState(initailFormValues);
+}
+
+
+
+
+export default CreateClass;
