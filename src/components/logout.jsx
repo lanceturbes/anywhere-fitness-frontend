b8cb149a8de8c6f4bcf84d4ab5696621e4129072
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-11-18 10:08:01 -0800

    fixed logout component

--- src/components/logout.jsx
+++ src/components/logout.jsx
@@ -1,22 +1,15 @@
-import React, {useEffect} from 'react';
-import {useNavigate} from 'react-router-dom';
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-import axiosWithAuth from '../utils/axiosWithAuth';
+const Logout = () => {
+  const navigate = useNavigate()
 
-const Logout = (props) => {  
-    const navigate = useNavigate();
+  useEffect(() => {
+    localStorage.removeItem('token')
+    navigate('/login')
+  }, [])
 
-    useEffect(()=> {
-        const token = localStorage.getItem("token");
-        
-        axiosWithAuth()
-            .post('/logout')
-            .then(resp => {                
-                localStorage.removeItem('token');
-                navigate('/login');
-            });
-    }, []);      
-    return(<div></div>);
+  return (<div></div>)
 }
 
-export default Logout;
+export default Logout
