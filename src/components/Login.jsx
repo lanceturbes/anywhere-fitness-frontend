330a47ba421fa1bb9d61de797fcae2fd51f9595d
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-11-18 11:27:33 -0800

    fixed login not requiring or saving auth

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,95 +1,96 @@
-import React, { useEffect, useState } from 'react';
-import {Link} from 'react-router-dom'; 
-import axios from 'axios';
-import * as yup from 'yup';
-import userSchema from '../validation/userSchema';
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import * as yup from 'yup'
+
+import userSchema from '../validation/userSchema'
 
 const initialValues = {
-    username:"",
-    password:""
-};
+  username: "",
+  password: ""
+}
 
 export default function Login() {
-    const [formValues, setFormValues] = useState(initialValues);
-    const [userError,setUserError] = useState(initialValues);
-    const [disabled,setDisabled] = useState(true);
+  const navigate = useNavigate()
 
-    const handleChange = (e) => {
-        validate(e.target.name,e.target.value)
-        setFormValues({
-            ...formValues,
-            [e.target.name]: e.target.value
-        });
-    };
-    const validate = (name,value) => {
-        yup.reach(userSchema,name).validate(value)
-            .then(() => setUserError({...userError,[name]:''}))
-            .catch(err =>{
-                setUserError({...userError,[name]:err.errors[0]})
-            })
-    }
-    
+  const [formValues, setFormValues] = useState(initialValues)
+  const [formErrors, setFormErrors] = useState(initialValues)
+  const [loginError, setLoginError] = useState("")
+  const [disabled, setDisabled] = useState(true)
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        axios.post ('https://anywhere-fitness-bwft5.herokuapp.com/api/auth/login', formValues)
-        .then((res) => {
-            window.localStorage.setItem('token', res.data.token);
-            
-        })
-        .catch(err => {
-            console.log(err.message);
-        })
-        .finally(() => {
-            setFormValues(formValues)
-        })
-    };
-    useEffect(()=>{
-        userSchema.isValid(formValues).then(valid => setDisabled(!valid))
- 
-    },[formValues])
-    
+  const handleChange = (e) => {
+    validate(e.target.name, e.target.value)
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value
+    })
+  }
 
-    return (
-        <>
-            <div id="card-content">
-                <form id="login-form" className="form" onSubmit={handleSubmit}>
-                    <div className="login-form-header">
-                        <h1>Login to view your classes.</h1>
-                    </div>
+  const validate = (name, value) => {
+    yup.reach(userSchema, name).validate(value)
+      .then(() => setFormErrors({ ...formErrors, [name]: '' }))
+      .catch(err => {
+        setFormErrors({ ...formErrors, [name]: err.errors[0] })
+      })
+  }
 
-                    <div className="form-container">
-                        <div className='input-container' id='login-inputs'>
-                            <label>Username:
-                            <input
-                                value={formValues.username}
-                                name="username"
-                                type="text"
-                                onChange={handleChange}
-                            /></label>
-                            <div className="errors">{userError.username}</div>
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axios.post('https://anywhere-fitness-bwft5.herokuapp.com/api/auth/login', formValues)
+      .then(res => {
+        localStorage.setItem('token', res.data.token)
+        localStorage.setItem("user_id", res.data.user.id)
+        navigate("/dashboard")
+      })
+      .catch(err => {
+        setLoginError("Invalid Login! Please try again.")
+      })
+  }
 
-                            <label>Password:
-                            <input
-                                value={formValues.password}
-                                name="password"
-                                type="password"
-                                onChange={handleChange}
-                            /></label>
-                            <div className="errors">{userError.password}</div>
-                        </div>
+  useEffect(() => {
+    userSchema.isValid(formValues).then(valid => setDisabled(!valid))
+  }, [formValues])
 
-                        
-                        <Link to={`/dashboard`}>
+  return (
+    <>
+      <div id="card-content">
+        <form id="login-form" className="form" onSubmit={handleSubmit}>
+          <div className="login-form-header">
+            <h1>Login to view your classes.</h1>
+            {loginError && <p>{loginError}</p>}
+          </div>
 
-                        <button className="btn" type="submit" disabled={disabled} >Login</button>
-                        </Link>
+          <div className="form-container">
+            <div className='input-container' id='login-inputs'>
+              <label>
+                Username:
+                <input
+                  value={formValues.username}
+                  name="username"
+                  type="text"
+                  onChange={handleChange}
+                />
+              </label>
+              <div className="errors">{formErrors.username}</div>
 
-                        <Link to={`/signup`}>
-                        <p>Don't have an account yet?</p></Link>
-                    </div>
-                </form>
+              <label>
+                Password:
+                <input
+                  value={formValues.password}
+                  name="password"
+                  type="password"
+                  onChange={handleChange}
+                />
+              </label>
+              <div className="errors">{formErrors.password}</div>
             </div>
-        </>
-    )
+
+            <button className="btn" type="submit" disabled={disabled}>Login</button>
+
+            <Link to={`/signup`}>Don't have an account yet?</Link>
+          </div>
+        </form>
+      </div>
+    </>
+  )
 }
