e8b3105937c11ae32203634fc3360961b1ab9ad8
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-11-18 11:27:48 -0800

    updated routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,26 @@
-import React,{useState, useEffect} from 'react';
-import { Routes, Route } from "react-router-dom";
-import axios from 'axios';
-import HomePage from "./components/HomePage";
-import Navbar from "./components/Navbar";
-import Login from "./components/Login";
-import SignUp from "./components/SignUp";
-import Logout from "./components/Logout";
+import React from 'react'
+import { Routes, Route } from "react-router-dom"
+
+import UserPage from "./components/UserPage"
+import HomePage from "./components/HomePage"
+import Navbar from "./components/Navbar"
+import Login from "./components/Login"
+import SignUp from "./components/SignUp"
+import Logout from "./components/Logout"
+
 import './styles/App.css'
-import UserPage from "./components/UserPage";
+
 function App() {
-  const [userData, setUserData] = useState([]);
-  const [disabled,setDisabled] = useState(true);
-
-
-
-useEffect(() => {
-    axios
-      .get("https://anywhere-fitness-bwft5.herokuapp.com/api/classes")
-      .then((res) => {
-        console.log(res.data);
-        return setUserData(res.data);
-      })
-      .catch((err) => {
-        console.log("uh-oh!");
-      });
-  }, []);
-
-
   return (
     <>
-      {/*
-        React Router was updated not too long ago to v6!
-        Syntax is different now; read here for more details:
-        https://reactrouter.com/docs/en/v6/upgrading/v5#refactor-custom-routes
-      */}
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path ="/" element={<HomePage />} />
-     
-        <Route path ="/signup" element={<SignUp disabled ={disabled} setDisabled={setDisabled}/>} />
-      
-        <Route path="/login" element={<Login disabled ={disabled} setDisabled={setDisabled}/>} />
-        
-        <Route path="/dashboard" element={<UserPage users={userData} />} />
-
-      </Routes>
-      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<UserPage />} />
         <Route path="/logout" element={<Logout />} />
       </Routes>
-        
-     
     </>
   )
 }
