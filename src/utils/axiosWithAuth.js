4c5db2224d8dce438c7688c9074a0b5377b01e44
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-11-18 11:25:18 -0800

    removed baseURL from axios with auth

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -8,8 +8,7 @@ const axiosWithAuth = () => {
   return axios.create({
     headers: {
       authorization: token
-    },
-    baseURL: 'https://anywhere-fitness-bwft5.herokuapp.com/api'
+    }
   })
 }
 
